d77d8c81a6549853dc9cd74b1eafa21efe83af28
BabayVadimovich <drakizvezdgame.tv@gmail.com>
2025-03-23 17:37:49 +0100

    🌐Proxy replacement

--- smscallbomber/service.py
+++ smscallbomber/service.py
@@ -1,6 +1,7 @@
 import random
 import string
 import requests
+import re
 import smscallbomber.config as cfg
 
 def username():
@@ -54,9 +55,62 @@ class Service:
             if old in str(self.json):
                 self.json = str(self.json).replace(old, new)
 
+    @staticmethod
+    def fetch_proxies(url):
+        try:
+            res = requests.get(url, timeout=3)
+            res.raise_for_status()
+            lines = res.text.splitlines()
+            proxies = [line.strip() for line in lines if re.match(r'^\d+\.\d+\.\d+\.\d+:\d+$', line.strip())]
+            
+            return proxies
+        except requests.RequestException:
+            return []
+
+    @staticmethod
+    def check_proxy(proxy):
+        try:
+            res = requests.get("http://ipinfo.io/json", proxies={"http": proxy, "https": proxy}, timeout=3)
+            return res.status_code == 200
+        except requests.RequestException:
+            return False
+
     def generate_proxy(self):
-        proxy = requests.get("https://gimmeproxy.com/api/getProxy?curl=true&get=true&cookies=true&referer=true&user-agent=true&minSpeed=50&post=true&protocol=http&supportsHttps=true&maxCheckPeriod=3600")
-        return {"http": proxy.text, "https": proxy.text}
+        proxy_sources = [
+            "https://api.openproxylist.xyz/http.txt",
+            "https://www.proxy-list.download/api/v1/get?type=http",
+            "https://raw.githubusercontent.com/officialputuid/KangProxy/KangProxy/http/http.txt",
+            "https://raw.githubusercontent.com/monosans/proxy-list/main/proxies/http.txt",
+            "https://api.proxyscrape.com/v2/?request=displayproxies&protocol=http&timeout=10000&country=all&ssl=all&anonymity=all",
+            "https://raw.githubusercontent.com/TheSpeedX/SOCKS-List/master/http.txt",
+            "https://www.sslproxies.org/",
+            "https://www.proxy-list.download/api/v1/get?type=https",
+            "https://api.proxyscrape.com/v2/?request=displayproxies&protocol=https&timeout=10000&country=all&ssl=all&anonymity=all",
+            "https://raw.githubusercontent.com/officialputuid/KangProxy/KangProxy/https/https.txt",
+            "https://raw.githubusercontent.com/jetkai/proxy-list/main/online-proxies/txt/proxies-https.txt",
+            'https://www.sslproxies.org/',
+            'https://www.google-proxy.net/',
+            'https://free-proxy-list.net/anonymous-proxy.html',
+            'https://free-proxy-list.net/uk-proxy.html',
+            'https://www.us-proxy.org/',
+            'https://api.proxyscrape.com/v2/?request=displayproxies&protocol=http&timeout=10000&country=all&ssl=all&anonymity=all',
+            'https://api.proxyscrape.com/v2/?request=displayproxies&protocol=https&timeout=10000&country=all&ssl=all&anonymity=all',
+            'https://free-proxy-list.net/'
+        ]
+
+        random.shuffle(proxy_sources)
+
+        for url in proxy_sources:
+            proxies = self.fetch_proxies(url)
+            random.shuffle(proxies)
+
+            for proxy in proxies:
+                if self.check_proxy(proxy):
+                    return {"http": proxy, "https": proxy}
+                else:
+                    break
+
+        return None
 
     def get_domain_name(self):
         website = self.service_info['info'].get('website', '')
